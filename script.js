0db7a6a998cee85e22e51fee61a8ac46c4d28abd
Ruchi2613 <ruchipandey2606@gmail.com>
2024-12-23 14:00:53 -0600

    Experience description and contact us

--- script.js
+++ script.js
@@ -5,20 +5,4 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () =>{
     navLinks.classList.toggle('active');
 }
-function sendMail() {
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    };
 
-    emailjs.send("service_w8cx29b", "template_09yf1zj", parms)
-        .then(function(response) {
-            alert("Email Sent Successfully!");
-            console.log('Success:', response);
-        }, function(error) {
-            alert("Failed to send email. Please try again.");
-            console.log('Error:', error);
-        });
-}
